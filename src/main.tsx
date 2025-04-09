8c7bb91ecaf4abb3bd288215afd656b83b195b4e
Matheus <matheusleidow@gmail.com>
2025-04-09 20:34:26 -0300

    Add Initial createStore

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+
+const store = createStore(() => {})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
